af1d96d1ee5145bceb4bf7e6c80df2fecbdad6de
ritco <rik@ritco.be>
2017-01-04 22:31:39 +0100

    admin aanpassing v2

--- schapen/admin.py
+++ schapen/admin.py
@@ -4,7 +4,6 @@ from django.contrib import admin
 
 # aanpassing is nodig om de schapen gegevens in de admin backend te zien
 
-from django.db.models import get_models, get_app
+from schapen.models import *
 
-for model in get_models(get_app('schapen')):
-    admin.site.register(model)
+admin.site.register(Schapen)
