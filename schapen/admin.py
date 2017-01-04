166416abd1ae293e47a54c99f93a014bac659e3f
ritco <rik@ritco.be>
2017-01-04 22:12:57 +0100

    schapen tonen in de admin backend

--- schapen/admin.py
+++ schapen/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# aanpassing is nodig om de schapen gegevens in de admin backend te zien
+
+from .models import Schapen
+
+class reviewSchapen(admin.ModelAdmin):
+        model = Review
+        list_display = ('schapen', 'usern_name', 'pub_date')
+        list_filter = ['pub_date', 'usern_name']
+
+admin.site.register(reviewSchapen)
