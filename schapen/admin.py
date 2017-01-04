6c71359120f2ed64ec111a65e9865f48b7b37bb3
ritco <rik@ritco.be>
2017-01-04 22:38:32 +0100

    admin schapen er weer uit

--- schapen/admin.py
+++ schapen/admin.py
@@ -5,10 +5,3 @@ from django.contrib import admin
 # aanpassing is nodig om de schapen gegevens in de admin backend te zien
 
 from schapen.models import *
-
-class SchapenAdmin(admin.ModelAdmin):
-    model = Review
-    list_display = ('intern_nummer', 'geslacht', 'geboortedatum','user_name')
-    list_filter = ['geslacht']
-
-admin.site.register(Schapen, SchapenAdmin)
