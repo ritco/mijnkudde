5abeead652f81b759b58d6094ec5857eab7ea2a7
ritco <rik@ritco.be>
2017-01-04 21:48:55 +0100

    aanmaken schapen model (tabel)

--- schapen/models.py
+++ schapen/models.py
@@ -3,3 +3,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Schapen(models.Model):
+    geslacht_keuzes = ((1, 'Ram'), (2, 'Ooi'))
+
+    intern_nummer = models.CharField(max-length=50)
+    geslacht = models.IntegerField(choices=geslacht_keuzes)
+    geboortedatum = models.DateTimeField()
+    einddatum = models.DateTimeField()
+    Sanitel = models.CharField(length=11)
