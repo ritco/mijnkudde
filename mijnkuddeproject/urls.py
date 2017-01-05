f1c6bb7566f9f5bed5fd041034535fc71cea105b
ritco <rik@ritco.be>
2017-01-05 08:25:58 +0100

    url aanpassing

--- mijnkuddeproject/urls.py
+++ mijnkuddeproject/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^schapen/', include('schapen.urls', namespace="schapen")),
+    # url(r'^schapen/', include('schapen.urls', namespace="schapen")),
     url(r'^admin/', admin.site.urls),
 ]
