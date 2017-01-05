9a99b3a7a93d9d66fdd54d8cf7286423508d7213
ritco <rik@ritco.be>
2017-01-05 17:41:11 +0100

    index response

--- schapen/views.py
+++ schapen/views.py
@@ -3,8 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 from .models import Schapen
+from django.http import HttpResponse
 
 def schapen_list(request):
     schapen_list = Schapen.objects.order_by('-intern_nummer')
     context = {'schapen_list':schapen_list}
     return render(request, 'schapen/schapen_list.html', context)
+
+def index(request):
+    return HttpResponse('Dit is de startpagina van MijnKudde.be')
