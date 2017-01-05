96da348a1719b1c6c46c016bbae82ae1a8406bf7
ritco <rik@ritco.be>
2017-01-05 09:53:33 +0100

    utf-8 toegevoegd

--- schapen/urls.py
+++ schapen/urls.py
@@ -1,4 +1,6 @@
-""" hier definiëren we alle URLs die beginnen met schapen """
+# hier definiëren we alle URLs die beginnen met schapen
+
+# coding=utf-8
 
 from django.conf.urls import url
 from . import views
