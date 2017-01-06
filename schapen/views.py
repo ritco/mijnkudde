5a4e54b2613215557fd8a0cd2371f5bda418de48
ritco <rik@ritco.be>
2017-01-06 14:59:21 +0100

    test fetchone

--- schapen/views.py
+++ schapen/views.py
@@ -11,7 +11,7 @@ from django.http import HttpResponse
 #    return render(request, 'schapen/schapen_list.html', context)
 
 def index(request):
-    schapen_overzicht[] = Schapen.objects.get()[:5]
-    output = ', '. join (str([p.intern_nummer for p in schapen_overzicht])
+    schapen_overzicht = Schapen.objects.fetchone()
+    # output = ', '. join (str([p.intern_nummer for p in schapen_overzicht])
     # output = "test van Rik"
     return HttpResponse(output)
