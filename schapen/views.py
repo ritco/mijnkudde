b6aa35aa7e28e1acc867655910d8e404deaa31b8
ritco <rik@ritco.be>
2017-01-06 13:52:25 +0100

    testjes

--- schapen/views.py
+++ schapen/views.py
@@ -5,12 +5,13 @@ from django.shortcuts import render
 from .models import Schapen
 from django.http import HttpResponse
 
-def schapen_list(request):
-    schapen_list = Schapen.objects.order_by('-intern_nummer')
-    context = {'schapen_list':schapen_list}
-    return render(request, 'schapen/schapen_list.html', context)
+# def schapen_list(request):
+#    schapen_list = Schapen.objects.order_by('-intern_nummer')
+#    context = {'schapen_list':schapen_list}
+#    return render(request, 'schapen/schapen_list.html', context)
 
 def index(request):
-    schapen_overzicht = Schapen.objects[:5]
-    output = ', '. join ([p.intern_nummer for p in schapen_overzicht])
+    # schapen_overzicht = Schapen.objects[:5]
+    # output = ', '. join ([p.intern_nummer for p in schapen_overzicht])
+    output = "test van Rik"
     return HttpResponse(output)
