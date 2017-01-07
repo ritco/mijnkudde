7e1597e8b83b2ba869e12ec7740c22740f1a81a9
ritco <rik@ritco.be>
2017-01-07 21:01:37 +0100

    filter aangepast

--- schapen/views.py
+++ schapen/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from schapen.models import Schapen
+from schapen.models import Schapen, Q
 from django.http import HttpResponse
 
 
@@ -11,6 +11,7 @@ from django.http import HttpResponse
 # view na login, alles met suffix bedrijf
 
 def bedrijf_index(request):
-    schapen_overzicht = Schapen.objects.all()[:3]
+    today = datetime.datetime.today()
+    schapen_overzicht = Schapen.objects.filter(Q(einddatum=today)|Q(einddatum=None))
     context = {'schapen_overzicht':schapen_overzicht}
     return render(request, 'bedrijf/index.html', context)
