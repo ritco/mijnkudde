ee5de9ba5ff1699ebd9ef4dcd5012fb8192c69b4
ritco <rik@ritco.be>
2017-01-07 21:45:48 +0100

    filter 5

--- schapen/views.py
+++ schapen/views.py
@@ -14,6 +14,6 @@ import datetime
 
 def bedrijf_index(request):
     today = datetime.datetime.today()
-    schapen_overzicht = Schapen.objects.filter(Q(einddatum__gt = today) | Q(einddatum = False ))
+    schapen_overzicht = Schapen.objects.filter(Q(einddatum__gt = today) | Q(einddatum__isnull = True ))
     context = {'schapen_overzicht':schapen_overzicht}
     return render(request, 'bedrijf/index.html', context)
