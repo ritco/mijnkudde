5c23ec8ebe41301024a14db6427c9664e7827203
ritco <rik@ritco.be>
2017-01-10 22:51:10 +0100

    context er uit

--- schapen/views.py
+++ schapen/views.py
@@ -16,20 +16,20 @@ import datetime
 def bedrijf_index(request):
     today = datetime.datetime.today()
     schapen_overzicht = Schapen.objects.filter(Q(einddatum__gt = today) | Q(einddatum__isnull = True )).order_by('intern_nummer')
-    context = {'schapen_overzicht':schapen_overzicht}
-    return render(request, 'bedrijf/index.html', context)
+    # context = {'schapen_overzicht':schapen_overzicht}
+    return render(request, 'bedrijf/index.html')
 
 
 def bedrijf_aanwezige_schapen(request):
     today = datetime.datetime.today()
     schapen_overzicht = Schapen.objects.filter(Q(einddatum__gt = today) | Q(einddatum__isnull = True )).order_by('intern_nummer')
-    context = {'schapen_overzicht':schapen_overzicht}
-    return render(request, 'bedrijf/aanwezige_schapen.html', context)
+    # context = {'schapen_overzicht':schapen_overzicht}
+    return render(request, 'bedrijf/aanwezige_schapen.html')
 
 def bedrijf_alle_schapen(request):
     schapen_overzicht = Schapen.objects.order_by('intern_nummer')
-    context = {'schapen_overzicht':schapen_overzicht}
-    return render(request, 'bedrijf/aanwezige_schapen.html', context)
+    # context = {'schapen_overzicht':schapen_overzicht}
+    return render(request, 'bedrijf/aanwezige_schapen.html')
 
 def bedrijf_schaap_toevoegen(request):
     form = AddSchapen
