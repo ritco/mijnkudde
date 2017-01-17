920ba474a676e25bd30daa4c7b693fcec3263147
ritco <rik@ritco.be>
2017-01-17 18:29:34 +0100

    name in orde zetten

--- schapen/urls.py
+++ schapen/urls.py
@@ -6,13 +6,15 @@ from django.conf.urls import url
 from . import views
 from schapen import views as schapen_views
 
+app_name = 'schapen'
+
 urlpatterns = [
     #voorbeeld: /
     url(r'^$', schapen_views.bedrijf_index),
     # voorbeeld: /schaap/internnr123
     url('aanwezige_schapen.html', schapen_views.bedrijf_aanwezige_schapen),
     url('alle_schapen.html', schapen_views.bedrijf_alle_schapen),
-    url('schaap_toevoegen.html', schapen_views.bedrijf_schaap_toevoegen, name='bedrijf_schaap_toevoegen'),
+    url('schaap_toevoegen/$', schapen_views.bedrijf_schaap_toevoegen, name='bedrijf_schaap_toevoegen'),
 
 
 ]
