98c4f5ee18ad309e42fbc8e3f3da8275c0124064
ritco <rik@ritco.be>
2017-01-22 12:43:47 +0100

    url

--- schapen/urls.py
+++ schapen/urls.py
@@ -10,11 +10,12 @@ app_name = 'schapen'
 
 urlpatterns = [
     #voorbeeld: /
-    url(r'^$', schapen_views.bedrijf_index),
+
     # voorbeeld: /schaap/internnr123
-    url(r'^aanwezige_schapen$', schapen_views.bedrijf_aanwezige_schapen, name = 'bedrijf_aanwezige_schapen'),
-    url(r'^alle_schapen$', schapen_views.bedrijf_alle_schapen, name = 'bedrijf_alle_schapen'),
-    url(r'^schaap_toevoegen$', schapen_views.bedrijf_schaap_toevoegen, name = 'bedrijf_schaap_toevoegen'),
+    url(r'^aanwezige_schapen', schapen_views.bedrijf_aanwezige_schapen, name = 'bedrijf_aanwezige_schapen'),
+    url(r'^alle_schapen', schapen_views.bedrijf_alle_schapen, name = 'bedrijf_alle_schapen'),
+    url(r'^schaap_toevoegen', schapen_views.bedrijf_schaap_toevoegen, name = 'bedrijf_schaap_toevoegen'),
+    url(r'^$', schapen_views.bedrijf_index, name='bedrijf_index'),
 
 
 ]
