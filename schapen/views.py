8d3e6ee84632766e3e5182d24bd69ae3482b61f1
ritco <rik@ritco.be>
2017-01-23 21:15:58 +0100

    login required import

--- schapen/views.py
+++ schapen/views.py
@@ -10,6 +10,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import redirect
 import datetime
 import forms
+from django.contrib.auth.decorators import login_required
 
 
 
