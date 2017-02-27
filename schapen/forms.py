e3d639604e3ed97e5473df39aa55aba4edce71bc
ritco <rik@ritco.be>
2017-02-27 16:45:49 +0100

    filter

--- schapen/forms.py
+++ schapen/forms.py
@@ -2,42 +2,16 @@ from django import forms
 from django.forms import extras
 from .models import Schapen
 from django.forms.fields import DateField
+from django.db.models import Q
 
 
-#class AddSchapen(forms.ModelForm):
-#
-#    class Meta:
-#        model = Schapen
-#        exclude = ('user', )
+class AddSchapen(forms.Form):
 
+   class Meta:
+        model = Schapen
+        exclude = ('owner', )
 
-class CustomModelForm(forms.ModelForm):
-    """
-    Sandboxes user data for each reference field pointing to an
-    object with an "owner" field.
-    """
-    def __init__(self, request, *args, **kwargs):
-        # cache the request object
-        self.request = request
-        init_result = super(CustomModelForm, self).__init__(*args, **kwargs)
-
-        # go through each field in this ModelForm
-        for field_name, field_widget in self.fields.iteritems():
-            # find the model field definition
-            model_field = getattr(self.Meta.model, field_name, None)
-            if model_field:
-                # check if it has a related model
-                related_model = getattr(model_field.field, 'related_model', None)
-                if related_model:
-                    # check if it has an owner field
-                    owner_field = getattr(related_model, 'owner', None)
-                    if owner_field:
-                        # filter field objects by owner = logged in user
-                        field_widget.queryset = related_model.objects.filter(owner=request.user)
-
-        return init_result
-
-
-
-class AddSchapen(CustomModelForm):
-    pass
+    def __init__(self, *args, **kwargs):
+          super(AddSchapen, self).__init__(*args, **kwargs)
+          self.fields['moeder'] = forms.ChoiceField(choices=[ (o.id, str(o)) for o in Schapen.objects.filter(owner=request.user).filter(geslacht=2)])
+          self.fields['vader'] = forms.ChoiceField(choices=[ (o.id, str(o)) for o in Schapen.objects.filter(owner=request.user).filter(geslacht=1)])
--- schapen/models.py
+++ schapen/models.py
@@ -21,19 +21,13 @@ class Schapen(models.Model):
     geboortedatum = models.DateField(null=True, blank=True)
     einddatum = models.DateField(null=True, blank=True)
     Sanitel = models.CharField(max_length=11)
-    moeder = models.ForeignKey(
-        'self',
+    moeder = models.CharField(
         blank=True,
         null=True,
-        related_name = 'ooi_lam',
-        limit_choices_to = {'geslacht': 2},
         )
-    vader = models.ForeignKey(
-        'self',
+    vader = models.CharField(
         blank=True,
         null=True,
-        related_name = 'ram_lam',
-        limit_choices_to={'geslacht': 1},
         )
     owner = models.ForeignKey(User, default=1, null=True)
     pub_datum = models.DateField(null=True, blank=True)
