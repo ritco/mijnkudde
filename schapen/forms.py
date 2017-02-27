dcc1da6b100f02f5e0e029600775e8aa2a7af596
ritco <rik@ritco.be>
2017-02-27 16:59:38 +0100

    tussenvariabele gebruiker

--- schapen/forms.py
+++ schapen/forms.py
@@ -10,8 +10,9 @@ class AddSchapen(forms.Form):
 
     def __init__(self, *args, **kwargs):
           super(AddSchapen, self).__init__(*args, **kwargs)
-          self.fields['moeder'] = forms.ChoiceField(choices=[ (o.id, str(o)) for o in Schapen.objects.filter(owner=self.request.user).filter(geslacht=2)])
-          self.fields['vader'] = forms.ChoiceField(choices=[ (o.id, str(o)) for o in Schapen.objects.filter(owner=self.request.user).filter(geslacht=1)])
+          gebruiker = request.user
+          self.fields['moeder'] = forms.ChoiceField(choices=[ (o.id, str(o)) for o in Schapen.objects.filter(owner=gebruiker).filter(geslacht=2)])
+          self.fields['vader'] = forms.ChoiceField(choices=[ (o.id, str(o)) for o in Schapen.objects.filter(owner=gebruiker).filter(geslacht=1)])
 
     class Meta:
         model = Schapen
