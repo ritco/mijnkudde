f68381567902a3ba6a6da56e378e892d5f9d4ef5
ritco <rik@ritco.be>
2017-02-27 14:57:52 +0100

    verplaatsen

--- schapen/forms.py
+++ schapen/forms.py
@@ -11,5 +11,33 @@ from django.forms.fields import DateField
 #        exclude = ('user', )
 
 
+class CustomModelForm(forms.ModelForm):
+    """
+    Sandboxes user data for each reference field pointing to an
+    object with an "owner" field.
+    """
+    def __init__(self, request, *args, **kwargs):
+        # cache the request object
+        self.request = request
+        init_result = super(CustomModelForm, self).__init__(*args, **kwargs)
+
+        # go through each field in this ModelForm
+        for field_name, field_widget in self.fields.iteritems():
+            # find the model field definition
+            model_field = getattr(self.Meta.model, field_name, None)
+            if model_field:
+                # check if it has a related model
+                related_model = getattr(model_field.field, 'related_model', None)
+                if related_model:
+                    # check if it has an owner field
+                    owner_field = getattr(related_model, 'owner', None)
+                    if owner_field:
+                        # filter field objects by owner = logged in user
+                        field_widget.queryset = related_model.objects.filter(owner=request.user)
+
+        return init_result
+
+
+
 class AddSchapen(CustomModelForm):
-    pass 
+    pass
