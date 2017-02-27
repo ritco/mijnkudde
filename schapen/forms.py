6c2c94cab4ec5c3edcee7f7acade19c24a9b1326
ritco <rik@ritco.be>
2017-02-27 17:06:53 +0100

    self

--- schapen/forms.py
+++ schapen/forms.py
@@ -6,13 +6,14 @@ from django.db.models import Q
 from django.contrib.auth.models import User
 
 
+
 class AddSchapen(forms.Form):
 
     def __init__(self, *args, **kwargs):
           super(AddSchapen, self).__init__(*args, **kwargs)
-          gebruiker = request.user
-          self.fields['moeder'] = forms.ChoiceField(choices=[ (o.id, str(o)) for o in Schapen.objects.filter(owner=gebruiker).filter(geslacht=2)])
-          self.fields['vader'] = forms.ChoiceField(choices=[ (o.id, str(o)) for o in Schapen.objects.filter(owner=gebruiker).filter(geslacht=1)])
+          gebruiker = self.request.user
+          self.fields['moeder'] = forms.ChoiceField(choices=[ (o.id, str(o)) for o in Schapen.objects.filter(owner = gebruiker).filter(geslacht=2)])
+          self.fields['vader'] = forms.ChoiceField(choices=[ (o.id, str(o)) for o in Schapen.objects.filter(owner = gebruiker).filter(geslacht=1)])
 
     class Meta:
         model = Schapen
