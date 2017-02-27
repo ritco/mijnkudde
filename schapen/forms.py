74aa910916f519107bdf19defe47fa7ffe133e42
ritco <rik@ritco.be>
2017-02-27 16:53:06 +0100

    user vergeten

--- schapen/forms.py
+++ schapen/forms.py
@@ -3,6 +3,7 @@ from django.forms import extras
 from .models import Schapen
 from django.forms.fields import DateField
 from django.db.models import Q
+from django.contrib.auth.models import User
 
 
 class AddSchapen(forms.Form):
